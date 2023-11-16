67fdd945ae0da8b68ec3f3519cff3275bf608666
amradhwen <radhweneamri9631@gmail.com>
2023-11-16 02:03:40 +0100

    .

---
+++ src/content_option.js
@@ -0,0 +1,176 @@
+const logotext = "AMRADHWEN";
+const meta = {
+    title: "Amri Radhwen",
+    description: "I’m Radhwen Amri information systems developer _ Full stack devloper,currently a freelancer",
+};
+
+const introdata = {
+    title: "I’m Radhwen Amri",
+    animated: {
+        first: "I love coding",
+        second: "I code almost everything ;)",
+        third: "I develop mobile apps, websites, desktop apps...etc",
+    },
+    description: "In our field of application we do not need a biography, just take your first steps and we do the rest, our know-how is our identity.",
+    your_img_url: "/cover.jpg",
+};
+
+const dataabout = {
+    title: "A bit about myself",
+    aboutme: "As a computer enthusiast, I learned to code from a young age in various computer languages ​​(Javascript, PHP, Python, Java, etc.) and I created personal projects. I master the different technical stages of creating a site or web application, mobile applications and desktop applications, from understanding user needs, to frontend and backend development, including maintenance.",
+};
+const worktimeline = [{
+        jobtitle: "Freelancer",
+        //where: "Localhost",
+        date: "2020",
+    },
+    {
+        jobtitle: "Full Stack Web Developer",
+        //where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Student & Desktop Developer",
+        //where: "ALquds",
+        date: "2018",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Java",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "Node",
+        value: 60,
+    },
+    {
+        name: "React",
+        value: 85,
+    },
+    {
+        name: "Angular",
+        value: 85,
+    },
+    {
+        name: "Spring",
+        value: 85,
+    }
+];
+
+const services = [{
+        title: "Website Development",
+        //description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Website Applications",
+        //description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Desktop Applications",
+        //description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Applications",
+        //description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    }
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "radhweneamri9631@gmail.com",
+    YOUR_FONE: "+216 26 794 992 / +216 23 901 324",
+    //description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_SERVICE_ID: "service_g1q2y4a",
+    YOUR_TEMPLATE_ID: "template_gu4bm9y",
+    YOUR_USER_ID: "ZA7vR5FuWazAb3Yud",
+};
+
+const socialprofils = {
+    github: "https://github.com/amradhwen",
+    facebook: "https://facebook.com/lifeisbada",
+    linkedin: "https://linkedin.com/in/radhwen-amri-9239b8209",
+    instagram: "https://instagram.com/am_radhwen",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
